bcfdf1f0a21f6a1effde172ef7b752f4125e1205
tejaswan kalluri <tejaswankalluri82522@gmail.com>
2021-07-01 15:27:09 +0530

    major update 1.0.0

--- src/components/grid.js
+++ src/components/grid.js
@@ -1,34 +1,58 @@
-import React, { useState, useEffect } from "react";
-import "../css/style.css";
-import "../css/card.css";
+import React, { useState, useEffect } from "react"
+import "../css/style.css"
+import "../css/card.css"
 export default function Grid() {
-  const [memes, setmemes] = useState([]);
+    const [memes, setmemes] = useState([])
+    const [errors, seterrors] = useState(false)
+    const Loadmore = () => {
+        fetch("https://meme-api.herokuapp.com/gimme/50")
+            .then((res) => res.json())
+            .then((data) => {
+                const newMemes = data.memes
+                newMemes.forEach((element) => {
+                    setmemes((meme) => [...meme, element])
+                })
+                seterrors(false)
+            })
+            .catch(() => seterrors(true))
+    }
+    useEffect(() => {
+        Loadmore()
+    }, [])
+    const Grids = memes.map((meme, i) => (
+        <li className="cards__item" key={i}>
+            <div className="card">
+                <img src={meme.preview[2]} alt={i} />
+                <div className="card__content">
+                    <a href={meme.preview[2]} target="_blank" rel="noreferrer">
+                        <button className="btn btn--block card__btn">
+                            Download
+                        </button>
+                    </a>
+                </div>
+            </div>
+        </li>
+    ))
 
-  useEffect(() => {
-    fetch("https://meme-api.herokuapp.com/gimme/50")
-      .then((res) => res.json())
-      .then((data) => {
-        setmemes(data.memes);
-        console.log(data.memes)
-      });
-  },[]);
-
-  const grids = memes.map((meme,i) => (
-    <li className="cards__item" key={i}>
-      <div className="card">
-        <img src={meme.preview[2]} alt={i} />
-        <div className="card__content">
-          <a href={meme.preview[2]} download>
-            <button className="btn btn--block card__btn">Download</button>
-          </a>
+    return (
+        <div className="carrr">
+            <ul className="cards">{!errors && Grids}</ul>
+            {!errors && (
+                <button
+                    className="btn btn--block card__btn load_more"
+                    onClick={() => Loadmore()}
+                >
+                    load more
+                </button>
+            )}
+            {errors && (
+                <div
+                    className="alert alert-danger alert-dismissible fade show"
+                    role="alert"
+                >
+                    Something went wrong
+                </div>
+            )}
         </div>
-      </div>
-    </li>
-  ));
-
-  return (
-    <div className="carrr">
-      <ul className="cards">{grids}</ul>
-    </div>
-  );
+    )
 }
